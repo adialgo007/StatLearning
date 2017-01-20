e9f1d227fbc4aae427e0ed587aeb31941769d284
adikumar007 <adialgo007@gmail.com>
2017-01-21 02:14:45 +0530

    Completed Intro.R

--- intro.R
+++ intro.R
@@ -1,5 +1,36 @@
 x=c(2,5,7)
 x
-y=x
+y=seq(from=4,length=3,by=3)
 y
-y*2
+x+y
+x/y
+x[2]
+x[2:3]
+x[-2]
+x[-c(1,2)]
+z=matrix(seq(1,12),4,3)
+z
+z[3:4,2:3]
+z[,2:3]
+z[,1]
+z[,1,drop=FALSE]
+dim(z)
+ls()
+rm(y)
+ls()
+x=runif(50)
+y=rnorm(50)
+plot(x,y)
+plot(x,y,xlab = "Random Uniform",ylab = "Random Normal",pch = "*",col="blue")
+par(mfrow = c(2,1))
+plot(x,y)
+hist(y)
+par(mfrow = c(1,1))
+Auto = read.csv("DataSets/Auto.csv")
+names(Auto)
+dim(Auto)
+summary(Auto)
+plot(Auto$cylinders,Auto$mpg)
+attach(Auto)
+search()
+plot(cylinders,mpg)
